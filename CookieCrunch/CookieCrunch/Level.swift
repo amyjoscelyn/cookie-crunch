ecd581c4c5c9bc4faa0d4f1aa9a7f601a2d43a4f
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-10-21 16:23:52 -0400

    Level.swift: cookies will never create a chain of three or more when the grid array is filled at the top of the game

--- CookieCrunch/CookieCrunch/Level.swift
+++ CookieCrunch/CookieCrunch/Level.swift
@@ -38,7 +38,16 @@ class Level
             {
                 if tiles[column, row] != nil
                 {
-                    let cookieType = CookieType.random()
+                    var cookieType: CookieType
+                    repeat
+                    {
+                        cookieType = CookieType.random()
+                    } while (column >= 2
+                        && cookies[column - 1, row]?.cookieType == cookieType
+                        && cookies[column - 2, row]?.cookieType == cookieType)
+                        || (row >= 2
+                            && cookies[column, row - 1]?.cookieType == cookieType
+                            && cookies[column, row - 2]?.cookieType == cookieType)
                     
                     let cookie = Cookie(column: column, row: row, cookieType: cookieType)
                     cookies[column, row] = cookie
